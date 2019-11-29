0d7f043cda2c441ac566ef63aa99aabcaa15402a
RevychMaksym <55156318+RevychMaksym@users.noreply.github.com>
2019-11-29 12:57:17 +0200

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -2,43 +2,45 @@
 
 #include <initializer_list>
 #include <iostream>
+#include <string>
 
 template <typename T>
 class Vector {
  public:
   Vector(size_t size);
-  // Vector(std::initializer_list<T>);
-  // Vector(T* begin, T* end, size_t size);
+  Vector(const std::initializer_list<T>& init_list);
+  Vector(const T* begin, const T* const end);
   Vector(const Vector<T>& object);
   const Vector<T>& operator=(const Vector<T>& object);
+  T& operator[](size_t index);
   ~Vector();
 
-  T begin() const;
-  T end() const;
+  T* begin();
+  T* end();
   void push_back(T value);
   void push_front(T value);
-  size_t insert(T value, unsigned pos);
+  size_t insert(T value, size_t pos);
 
-  // T* erase(size_t pos);
-  // T* erase(int* pos);
-  // T* erase(int* begin, int* end);
+  T* erase(size_t pos);
+  T* erase(T* pos);
+  T* erase(T* begin, T* end);
 
-  T* back() const;
-  T* front() const;
+  T back() const;
+  T front() const;
   void resize(size_t new_size);
 
-  void show() const;
+  void print() const;
   void clear();
+
+  size_t getsize() const { return _size; }
+  size_t getcapacity() const { return _capacity; }
+  bool empty() const;
+
+ private:
   bool needs_alloc() const;
   void allocate_memory();
 
-  size_t getsize() { return _size; }
-  size_t getcapacity() { return _capacity; }
-  bool empty() { return _is_empty; }
-
- private:
   T* _data;
   size_t _size;
   size_t _capacity;
-  bool _is_empty;
 };
