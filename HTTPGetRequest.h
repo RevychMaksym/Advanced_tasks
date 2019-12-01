8a8fd045ae9dc8532cc3a55b1e4806b215653ee5
RevychMaksym <55156318+RevychMaksym@users.noreply.github.com>
2019-12-01 10:08:55 +0200

    Formated to google style

--- HTTPGetRequest.h
+++ HTTPGetRequest.h
@@ -3,36 +3,32 @@
 #include <string>
 
 #include <boost/asio.hpp>
-typedef void(*HTTPRequestDataReceived)(char*, size_t);
-typedef void(*HTTPRequestComplete)();
+typedef void (*HTTPRequestDataReceived)(char*, size_t);
+typedef void (*HTTPRequestComplete)();
 
-class HTTPGetRequest
-{
-public:
-	HTTPGetRequest(
-		boost::asio::io_service& io_service,
-		std::string host,
-		std::string relativeURL,
-		HTTPRequestDataReceived receivedCB,
-		HTTPRequestComplete completeCB);
+class HTTPGetRequest {
+ public:
+  HTTPGetRequest(boost::asio::io_service& io_service, std::string host,
+                 std::string relativeURL, HTTPRequestDataReceived receivedCB,
+                 HTTPRequestComplete completeCB);
 
-	~HTTPGetRequest();
+  ~HTTPGetRequest();
 
-	void sendRequest();
+  void sendRequest();
 
-private:
-	HTTPRequestDataReceived m_receivedCB;
-	HTTPRequestComplete m_completeCB;
+ private:
+  HTTPRequestDataReceived m_receivedCB;
+  HTTPRequestComplete m_completeCB;
 
-	std::string m_host;
-	std::string m_relativeURL;
+  std::string m_host;
+  std::string m_relativeURL;
 
-	boost::asio::ip::tcp::socket m_socket;
-	boost::asio::io_service& m_io_service;
-	boost::asio::ip::tcp::resolver m_resolver;
+  boost::asio::ip::tcp::socket m_socket;
+  boost::asio::io_service& m_io_service;
+  boost::asio::ip::tcp::resolver m_resolver;
 
-	boost::asio::streambuf m_request;
-	boost::asio::streambuf m_response;
+  boost::asio::streambuf m_request;
+  boost::asio::streambuf m_response;
 
-	void ReadData();
+  void ReadData();
 };
