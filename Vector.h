42ad3bdc0d9937ddb5b297f47e9dbcfa9a70e4de
RevychMaksym <55156318+RevychMaksym@users.noreply.github.com>
2019-12-02 21:42:45 +0200

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -6,44 +6,45 @@
 
 template <typename T>
 class Vector {
-public:
-	Vector(size_t size);
-	Vector(const std::initializer_list<T>& init_list);
-	Vector(const T* begin, const T* const end);
-	Vector(const Vector<T>& object);
-	const Vector<T>& operator=(const Vector<T>& object);
-	T& operator[](size_t index);
-	~Vector();
+ public:
+  Vector(size_t size);
+  Vector(const std::initializer_list<T>& init_list);
+  Vector(const T* begin, const T* const end);
+  Vector(const Vector<T>& object);
+  const Vector<T>& operator=(const Vector<T>& object);
+  T& operator[](size_t index);
+  ~Vector();
 
-	T* begin();
-	T* end();
-	void push_back(T value);
-	void push_front(T value);
-	size_t insert(T value, size_t pos);
-	
-	T* erase(size_t pos);
-	T* erase(T* pos);
-	T* erase(T* begin, T* end);
+  T* begin();
+  T* end();
+  void push_back(T value);
+  void push_front(T value);
+  size_t insert(T value, size_t pos);
 
-	T back() const;
-	T front() const;
-	void resize(size_t new_size);
-	
-	void print() const;
-	void clear();
+  T* erase(size_t pos);
+  T* erase(T* pos);
+  T* erase(T* begin, T* end);
 
-	size_t getsize() const { return _size; }
-	size_t getcapacity() const { return _capacity; }
-	bool empty() const;
-private:
-	bool needs_alloc() const;
-	void allocate_memory();
-	void initialise_object(const Vector<T>& object);
+  T back() const;
+  T front() const;
+  void resize(size_t new_size);
 
-	T* eraser(T* begin, size_t counter);
-	void get_object(const T* begin, const T* const end);
+  void print() const;
+  void clear();
 
-	T* _data;
-	size_t _size;
-	size_t _capacity;
+  size_t getsize() const { return _size; }
+  size_t getcapacity() const { return _capacity; }
+  bool empty() const;
+
+ private:
+  bool needs_alloc() const;
+  void allocate_memory();
+  void initialise_object(const Vector<T>& object);
+
+  T* eraser(T* begin, size_t counter);
+  void get_object(const T* begin, const T* const end);
+
+  T* _data;
+  size_t _size;
+  size_t _capacity;
 };
